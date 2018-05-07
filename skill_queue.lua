bd5f028ec5dda0fcbde61d1c966f67138d6f7b41
Alex-Guest <master_kenno@hotmail.co.uk>
2018-05-07 14:10:43 +0100

    Updated skill queue to account for multiple rank changes.

--- skill_queue.lua
+++ skill_queue.lua
@@ -2,12 +2,29 @@ require("skill_queue_util");
 CharacterSkillQueue = require("character_skill_queue");
 SkillQueueManager = require("skill_queue_manager");
 
-local skillQueueManager = SkillQueueManager.new();
-local testCqi = 31;
---# assume testCqi: CA_CQI
-local testQueue = CharacterSkillQueue.new(testCqi);
-testQueue:addSkillToQueue("wh2_main_skill_hef_noble_unique_combat");
-testQueue:addSkillToQueue("wh2_main_skill_hef_combat_valour_of_ages");
-testQueue:addSkillToQueue("wh2_main_skill_hef_combat_valour_of_ages");
-testQueue:addSkillToQueue("wh_main_skill_all_all_self_hard_to_hit");
-skillQueueManager.characterSkillQueues[testCqi]  = testQueue;
+function startSkillQueue()
+    output("Skill queue started");
+    local skillQueueManager = SkillQueueManager.new();
+    local testCqi = 31;
+    --# assume testCqi: CA_CQI
+    local testQueue = CharacterSkillQueue.new(testCqi);
+    testQueue:addSkillToQueue("wh2_main_skill_hef_noble_unique_combat");
+    testQueue:addSkillToQueue("wh2_main_skill_hef_combat_valour_of_ages");
+    testQueue:addSkillToQueue("wh2_main_skill_hef_combat_valour_of_ages");
+    testQueue:addSkillToQueue("wh_main_skill_all_all_self_hard_to_hit");
+    skillQueueManager.characterSkillQueues[testCqi]  = testQueue;
+end
+
+core:add_listener(
+    "SkillQueueStartListener",
+    "FirstTickAfterWorldCreated",
+    function(context)
+        return true;
+    end,
+    function(context)
+        -- No idea why this is needed
+        _G.get_faction = get_faction;
+        startSkillQueue();
+    end,
+    true
+);
--- skill_queue_manager.lua
+++ skill_queue_manager.lua
@@ -1,9 +1,10 @@
 local SkillQueueManager = {} --# assume SkillQueueManager: SKILL_QUEUE_MANAGER
 SkillQueueManager.__index = SkillQueueManager;
 SkillQueueManager.characterSkillQueues = {} --: map<CA_CQI, CHARACTER_SKILL_QUEUE>
+SkillQueueManager.characterRanks = {} --: map<CA_CQI, integer>
 
---v function(self: SKILL_QUEUE_MANAGER, character: CA_CHAR, skill: string)
-function SkillQueueManager.allocateSkill(self, character, skill)
+--v function(self: SKILL_QUEUE_MANAGER, character: CA_CHAR, skill: string, successCallback: function())
+function SkillQueueManager.allocateSkill(self, character, skill, successCallback)
     local skillAllocated = false;
     cm:callback(
         function()
@@ -13,6 +14,7 @@ function SkillQueueManager.allocateSkill(self, character, skill)
             else
                 output("Skill allocated");
                 self.characterSkillQueues[character:cqi()]:skilledInto(skill);
+                successCallback();
             end
         end, 0, "SkillAllocationCallback"
     );
@@ -30,9 +32,13 @@ function SkillQueueManager.allocateSkill(self, character, skill)
     cm:force_add_skill("character_cqi:" .. tonumber(character:cqi()), skill);
 end
 
---v function(self: SKILL_QUEUE_MANAGER, character: CA_CHAR)
-function SkillQueueManager.processCharRankedUp(self, character)
+--# assume SKILL_QUEUE_MANAGER.processCharRankedUp: function(self: SKILL_QUEUE_MANAGER, character: CA_CHAR, ranks: integer)
+--v function(self: SKILL_QUEUE_MANAGER, character: CA_CHAR, ranks: integer)
+function SkillQueueManager.processCharRankedUp(self, character, ranks)
     output("Char ranked up: " .. tonumber(character:cqi()));
+    if ranks == 0 then
+        return;
+    end
     local characterSkillQueue = self.characterSkillQueues[character:cqi()];
     if not characterSkillQueue then
         return;
@@ -41,7 +47,12 @@ function SkillQueueManager.processCharRankedUp(self, character)
     if not nextSkill then
         return;
     end
-    self:allocateSkill(character, nextSkill);
+    self:allocateSkill(
+        character, nextSkill,
+        function()
+            self:processCharRankedUp(character, ranks - 1);
+        end
+    );
 end
 
 --v function(self: SKILL_QUEUE_MANAGER)
@@ -54,10 +65,23 @@ function SkillQueueManager.registerForCharRankUp(self)
         end,
         function(context)
             local rankedUpChar = context:character();
-            self:processCharRankedUp(rankedUpChar);
+            local rankDiff = rankedUpChar:rank() - self.characterRanks[rankedUpChar:cqi()];
+            output("char rank diff : " .. rankDiff);
+            self.characterRanks[rankedUpChar:cqi()] = rankedUpChar:rank();
+            self:processCharRankedUp(rankedUpChar, rankDiff);
         end,
         true
     );
+end
+
+--v function(self: SKILL_QUEUE_MANAGER)
+function SkillQueueManager.calculateCharacterRanks(self)
+    local currentFaction = get_faction(cm:get_local_faction());
+    local characters = currentFaction:character_list();
+    for i = 0, characters:num_items() - 1 do
+        local currentCharacter = characters:item_at(i);
+        self.characterRanks[currentCharacter:cqi()] = currentCharacter:rank();
+    end
 end
 
 --v function() --> SKILL_QUEUE_MANAGER
@@ -66,6 +90,7 @@ function SkillQueueManager.new()
     setmetatable(sqm, SkillQueueManager);
     --# assume sqm: SKILL_QUEUE_MANAGER
     sqm:registerForCharRankUp();
+    sqm:calculateCharacterRanks();
     return sqm
 end
 
