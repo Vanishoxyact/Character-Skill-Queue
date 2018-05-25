10c920db8d9c2cc6c47763e10bf23d72c136f7b3
Alex-Guest <master_kenno@hotmail.co.uk>
2018-05-25 22:24:22 +0100

    Added potential fix for missing global methods.

--- export_helpers_vanish_skill_queue.lua
+++ export_helpers_vanish_skill_queue.lua
@@ -2,3 +2,7 @@ require("skill_queue");
 -- No idea why this is needed
 _G.get_faction = get_faction;
 _G.get_character_by_cqi = get_character_by_cqi;
+
+-- General
+--module(..., package.seeall)
+--_G.main_env = getfenv(1) -- Probably not needed in most places
